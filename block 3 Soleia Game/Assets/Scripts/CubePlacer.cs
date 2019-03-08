071fe41e4cca91e9bcb12d2339f3619d9dc214d8
Moobesoor <edorb721@hotmail.com>
2019-03-08 10:56:13 +0100

    simple grid
    simple grid

---
+++ block 3 Soleia Game/Assets/Scripts/CubePlacer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubePlacer : MonoBehaviour
+{
+    private Grid grid;
+
+    GameObject Randomobject; 
+
+    private void Awake()
+    {
+        grid = FindObjectOfType<Grid>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hitInfo;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hitInfo))
+            {
+                PlaceCubeNear(hitInfo.point);
+            }
+        }
+    }
+
+    private void PlaceCubeNear(Vector3 clickPoint)
+    {
+        var finalPosition = grid.GetNearestPointOnGrid(clickPoint);
+        GameObject.CreatePrimitive(Randomobject).transform.position = finalPosition;
+
+        //GameObject.CreatePrimitive(PrimitiveType.Sphere).transform.position = clickPoint;
+    }
+}
