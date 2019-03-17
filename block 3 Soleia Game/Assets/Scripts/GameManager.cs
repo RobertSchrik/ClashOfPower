9a649bc72ca3708c04e8689c83a55e7f64f7c1b7
Moobesoor <edorb721@hotmail.com>
2019-03-17 13:06:17 +0100

    quiz  achtig iets

---
+++ block 3 Soleia Game/Assets/Scripts/GameManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+
+    public Question[] questions;
+    private static List<Question> unansweredQuestions;
+
+    private Question currentQuestion;
+
+    [SerializeField]
+    private Text
+
+   void Start()
+    {
+        
+        if (unansweredQuestions == null || unansweredQuestions.Count == 0)
+        {
+            unansweredQuestions = questions.ToList<Question>(); 
+        }
+
+        SetCurrentQuestion();
+       
+    }
+
+    void SetCurrentQuestion()
+    {
+        int randomQuestionIndex = Random.Range(0, unansweredQuestions.Count);
+        currentQuestion = unansweredQuestions[randomQuestionIndex];
+
+        currentQuestion.question;
+
+        unansweredQuestions.RemoveAt(randomQuestionIndex);
+    }
+
+}
